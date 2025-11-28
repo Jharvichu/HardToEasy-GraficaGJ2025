d2da42b76005cc548eb1d2d0de00fd827e32cccd
Jharvy <jharvy.cadillo.t@uni.pe>
2025-11-28 03:57:22 -0500

    feat: agregar audio a la UI

--- Assets/_Game/Scripts/UI/GamePlay/GameUIController.cs
+++ Assets/_Game/Scripts/UI/GamePlay/GameUIController.cs
@@ -14,12 +14,18 @@ public class GameUIController : MonoBehaviour
     private Button _btnPause;
     private VisualElement _menuPauseOverlay, _fadeOverlay;
     private Button _btnContinue, _btnReset, _btnQuit;
+    private Slider _volumenSlider;
 
     [SerializeField] private float _transitionTime;
 
     void Awake()
     {
         _doc = GetComponent<UIDocument>();
+    }
+
+    void Start()
+    {
+        AudioManager.Instance.UpdateBGMusic("Gameplay despierto");
     }
 
     void OnEnable()
@@ -43,6 +49,7 @@ public class GameUIController : MonoBehaviour
         _btnPause = root.Q<Button>("BtnPausarHUD");
 
         _menuPauseOverlay = root.Q<VisualElement>("MenuPausa");
+        _volumenSlider = root.Q<Slider>("SliderVolumen");
         _btnContinue = root.Q<Button>("BtnContinuar");
         _btnReset = root.Q<Button>("BtnReiniciar");
         _btnQuit = root.Q<Button>("BtnSalirMenu");
@@ -58,6 +65,12 @@ public class GameUIController : MonoBehaviour
         _btnContinue.clicked += CloseOptions;
         _btnReset.clicked += SceneReset;
         _btnQuit.clicked += SceneQuit;
+
+        if (_volumenSlider != null) 
+        {
+            _volumenSlider.value = AudioManager.Instance.GetGeneralVolume();
+            _volumenSlider.RegisterValueChangedCallback(OnVolumeChanged);
+        }
     }
 
     private void UnregisterEvents()
@@ -66,16 +79,20 @@ public class GameUIController : MonoBehaviour
         if( _btnContinue != null ) _btnContinue.clicked -= CloseOptions;
         if( _btnReset != null ) _btnReset.clicked -= SceneReset;
         if( _btnQuit != null ) _btnQuit.clicked -= SceneQuit;
+
+        if (_volumenSlider != null) _volumenSlider.UnregisterValueChangedCallback(OnVolumeChanged);
     }
 
     private void SceneReset()
     {
+        AudioManager.Instance.Play("Boton");
         GameManager.Instance.StartGame();
         StartCoroutine(SceneLoad("Office_1"));   
     }
 
     private void SceneQuit()
     {
+        AudioManager.Instance.Play("Boton");
         GameManager.Instance.StartGame();
         StartCoroutine(SceneLoad("Menu"));   
     }
@@ -84,19 +101,29 @@ public class GameUIController : MonoBehaviour
     {
         if (_fadeOverlay != null) _fadeOverlay.AddToClassList("cortina-cerrada");
         yield return new WaitForSeconds(_transitionTime);
+        AudioManager.Instance.StopBGM();
         SceneManager.LoadScene(nameScene);
     }
 
     private void OpenOptions()
     {
+        AudioManager.Instance.Play("Boton");
         GameManager.Instance.PauseGame();
         _menuPauseOverlay.RemoveFromClassList("oculto");
     }
 
     private void CloseOptions()
     {
+        AudioManager.Instance.Play("Boton");
         GameManager.Instance.StartGame();
         _menuPauseOverlay.AddToClassList("oculto");
     }
 
+    private void OnVolumeChanged(ChangeEvent<float> evt)
+    {
+        float valueSlider = evt.newValue;
+        float volumenNormalizado = valueSlider / 100f;
+
+        AudioManager.Instance.UpdateGeneralVolume(volumenNormalizado);
+    }
 }
